e033129a35b80f28f1ff1bd936d5181fa7488ede
sschr15 <steven.sschr15@hotmail.com>
2022-09-26 21:24:19 -0500

    add too many log

---
+++ src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/ConsoleLogExtension.kt
@@ -0,0 +1,38 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package org.quiltmc.community.modes.quilt.extensions
+
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.core.event.gateway.ReadyEvent
+import mu.KotlinLogging
+
+class ConsoleLogExtension : Extension() {
+	private val logger = KotlinLogging.logger {}
+
+	override val name = "console-logging"
+
+	override suspend fun setup() {
+		event<ReadyEvent> {
+			action {
+				logger.warn("GLOBAL COMMANDS")
+				kord.getGlobalApplicationCommands().collect {
+					logger.info("${it.name} - ${it.id}")
+				}
+
+				kord.guilds.collect { guild ->
+					logger.warn("GUILD: ${guild.name}")
+					guild.getApplicationCommands().collect {
+						logger.info("${it.name} - ${it.id}")
+					}
+				}
+
+				logger.warn("DONE")
+			}
+		}
+	}
+}
